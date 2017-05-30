c209eef270ca699a2a1bd9e22a6859882b250546
sinaiyuc <sinai.yunker@gmail.com>
2017-05-30 13:43:52 -0700

    asdf'

--- index.php
+++ index.php
@@ -14,43 +14,43 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<div id="primary" class="content-area">
+	<span class='myName'>Cooper Sinai-Yunker</span>
+	<span class='myDesc'>UX and Visual Designer</span>
+	<?php
+        //remove_all_filters('posts_orderby');
+        $args = array(
+            'posts_per_page' => 100,
+            'orderby'        => 'rand'
+        );
+        $query = new WP_Query($args);
+        $featuredimage = get_field_objects();
+    ?>
 
-		<?php
-		if ( have_posts() ) :
+	<div class='container-fluid'>
+    <?php if( $query->have_posts() ): ?>
+        <div class='row'>
+        <?php while ($query->have_posts()) : $query->the_post(); ?>
 
-			if ( is_home() && ! is_front_page() ) : ?>
-				<header>
-					<h1 class="page-title screen-reader-text"><?php single_post_title(); ?></h1>
-				</header>
+			<div class="col-lg-4 col-md-12 homepage-item project" >
+				<a class='infoLink' href='<?php the_permalink() ?>'>
+					<div class='contentCenter projects'>
+						<?php $heroImage = get_field('hero_image') ?>
+   			         	<img class='projectImage' src='<?php echo $heroImage['url'] ?>' />
+						<span class='titleHome'><?php the_title() ?></span>
+					</div>
+				</a>
+			</div>
 
-			<?php
-			endif;
+        <?php wp_reset_query(); ?>
 
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
-
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
-
-		endif; ?>
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+        <?php endwhile; ?>
+        </div>
+    <?php else : ?>
+        Hello
+    <?php endif; ?>
+	</div>
+</div><!-- #primary -->
 
 <?php
-get_sidebar();
 get_footer();
--- single.php
+++ single.php
@@ -6,30 +6,180 @@
  *
  * @package sinaiyuc_portfolio
  */
+?>
 
-get_header(); ?>
+ <?php get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+<a href="/" class='backButton'>
+	Home
+</a>
 
-		<?php
-		while ( have_posts() ) : the_post();
+ <div class='container-fluid projectPage'>
 
-			get_template_part( 'template-parts/content', get_post_format() );
+     <div class='heroImage'>
+         <?php $heroImage = get_field('hero_image') ?>
+         <img class='hero' src='<?php echo $heroImage['url'] ?>' />
+     </div>
 
-			the_post_navigation();
+     <div class='container'>
 
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+         <h1> <?php the_field('project_title') ?> </h1>
 
-		endwhile; // End of the loop.
-		?>
+         <h2>
+             <?php $fields = get_field('project_type') ?>
+             <?php if( $fields ): ?>
+                 <?php echo implode(' | ', $fields); ?>
+             <?php endif; ?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
+         </h2>
 
-<?php
-get_sidebar();
-get_footer();
+         <div class='row'>
+             <div class='overview col-md-6'><?php the_field('project_overview') ?> </div>
+         </div>
+
+         <div class='basicInfo row'>
+             <div class='smallInfo col-md-2'>
+
+                 <div class='timeframe'>
+                     <span class='title'>Timeframe</span>
+                     <span class='info'><?php the_field('timeframe') ?></span>
+                 </div>
+
+                 <!--Adding your roles-->
+                 <div class='roles'>
+                     <span class='title'>Roles</span>
+                 <?php if( have_rows('roles') ): ?>
+                     <?php while ( have_rows('roles') ) : the_row(); ?>
+                         <span class='info'>
+                             <?php the_sub_field('role'); ?>
+                         </span>
+                     <?php endwhile; ?>
+                 <?php else : ?>
+                     <!--// no rows found-->
+                 <?php endif; ?>
+                 </div>
+
+                 <!--Adding your skills-->
+                 <div class='skills'>
+                     <span class='title'>Skills</span>
+                 <?php if( have_rows('skills') ): ?>
+                     <?php while ( have_rows('skills') ) : the_row(); ?>
+                         <span class='info'>
+                             <?php the_sub_field('skill'); ?>
+                         </span>
+                     <?php endwhile; ?>
+                 <?php else : ?>
+                     <!--// no rows found-->
+                 <?php endif; ?>
+                 </div>
+
+
+                 <!--Adding your collaborators-->
+                 <div class='collaborators'>
+                     <span class='title'>Collaborators</span>
+                 <?php if( have_rows('collaborators') ): ?>
+                     <?php while ( have_rows('collaborators') ) : the_row(); ?>
+                         <span class='info'>
+                             <a href="<?php the_sub_field('their_site'); ?>"><?php the_sub_field('person'); ?></a>
+                         </span>
+                     <?php endwhile; ?>
+                 <?php else : ?>
+                     <!--// no rows found-->
+                 <?php endif; ?>
+                 </div>
+             </div>
+             <div class='col-md-1'></div>
+             <div class='largeInfo col-md-6'>
+                 <div class='problem'>
+                     <span class='title'>Problem</span>
+                     <span class='info'><?php the_field('problem') ?></span>
+                 </div>
+                 <div class='goal'>
+                     <span class='title'>Goal</span>
+                     <span class='info'><?php the_field('goal') ?></span>
+                 </div>
+                 <div class='solution'>
+                     <span class='title'>Solution</span>
+                     <span class='info'><?php the_field('solution') ?></span>
+                 </div>
+             </div>
+         </div>
+     </div>
+
+
+     <!-- Sections -->
+     <div class='section'>
+     <?php if( have_rows('sections') ): ?>
+         <?php while ( have_rows('sections') ) : the_row(); ?>
+             <?php if( get_row_layout() == 'section' ): ?>
+
+                 <div class='container'>
+                     <div class='row'>
+                         <div class='col-md-6'>
+                             <span class='sectionTitle'><?php the_sub_field('section_title') ?></span>
+                         </div>
+                     </div>
+                 </div>
+
+                 <!-- BEGIN INDIVIDUAL SECTION -->
+                 <?php if( have_rows('section_fields') ): ?>
+                     <?php while ( have_rows('section_fields') ) : the_row(); ?>
+                         <?php if( get_row_layout() == 'text_block' ): ?>
+
+                             <div class='container'>
+                                 <div class='row'>
+                                     <div class='col-md-6'>
+                                         <span class='textBlock'><?php the_sub_field('text_block') ?>
+                                     </div>
+                                 </div>
+                             </div>
+
+
+                         <?php elseif( get_row_layout() == 'image' ): ?>
+                             <div class='container'>
+                                     <img class='image' src='<?php echo the_sub_field('image') ?>' />
+                             </div>
+
+                         <?php elseif( get_row_layout() == 'quote' ): ?>
+                             <div class='container-fluid'>
+                                 <div class='row quoteRow'>
+                                         <span class='quote'><?php the_sub_field('quote') ?></span>
+                                 </div>
+                             </div>
+
+                         <?php elseif( get_row_layout() == 'subhead' ): ?>
+                             <div class='container'>
+                                 <div class='row'>
+                                     <div class='col-md-6'>
+                                         <span class='subhead'><?php the_sub_field('subhead') ?></span>
+                                     </div>
+                                 </div>
+                             </div>
+
+                         <?php elseif( get_row_layout() == 'subhead' ): ?>
+                             <div class='container'>
+                                 <div class='row'>
+                                     <div class='col-md-6'>
+                                         <span class='subhead'><?php the_sub_field('subhead') ?></span>
+                                     </div>
+                                 </div>
+                             </div>
+
+                         <?php endif; ?>
+                     <?php endwhile; ?>
+                 <?php else : ?>
+                 <!-- nothing found -->
+                 <?php endif; ?>
+
+
+             <?php endif; ?>
+         <?php endwhile; ?>
+     <?php else : ?>
+        <!-- nothing found -->
+     <?php endif; ?>
+     </div>
+
+
+ </div>
+
+ <?php get_footer(); ?>
